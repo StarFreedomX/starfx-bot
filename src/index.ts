6a92804c8d6bddf54c38b4b82549ff4feb102839
StarFreedomX <starfreedomx@outlook.com>
2025-04-16 23:41:33 +0800

    采用session.element遍历type法彻底解决字符串注入问题

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {Context, h, Schema, Session, Universal} from 'koishi'
-import {Jimp, JimpInstance} from 'jimp';
+import {Context, h, Schema, Session} from 'koishi'
+import {Jimp} from 'jimp';
 //import { Sharp } from 'sharp'
 import * as fs from 'fs'
 
@@ -14,7 +14,6 @@ export interface Config {
 export const Config: Schema<Config> = Schema.object({
   openLock: Schema.boolean().default(true).description('开启明日方舟封印功能'),
   openSold: Schema.boolean().default(true).description('开启闲鱼"卖掉了"功能'),
-
 })
 
 export const usage = 'StarFreedomX的自用插件 放了一些小功能'
@@ -36,46 +35,49 @@ export function apply(ctx: Context, cfg: Config) {
   if (cfg.openSold) {
     ctx.command('卖掉了 [param]')
     .action(async ({session}, param) => {
-      console.log('content:');
-      console.log(session.content);
-      return await drawSold(await getImageSrc(session, session.content));
+      console.log('elements');
+      console.log(session.elements);
+      console.log('quote');
+      console.log(session.quote);
+      console.log('param');
+      console.log(param);
+      //console.log(session.selfId);
+      return await drawSold(await getImageSrc(session, param));
     })
   }
 }
 
 export async function getImageSrc(session: Session, param: string){
-  let imageSrc = '';
-  //console.log(param);
-  if (!param){
-    console.log('no')
-    imageSrc = `https://q1.qlogo.cn/g?b=qq&nk=${session.userId}&s=640`;
-  }else if(param.startsWith('<at id=')){
-    console.log('at')
-    const userid = param.match(/<at\s+id="(\d+)"\s*\/?>/i)?.[1] ?? null;
-    imageSrc = `https://q1.qlogo.cn/g?b=qq&nk=${userid}&s=640`;
-  }else if(param.startsWith('<img src=')){
-    console.log('img')
-    const imgSrc =  param.match(/<img[^>]*\s+src="([^"]+)"/i)?.[1] ?? null;
-    if (imgSrc.startsWith('https://multimedia.nt.qq.com.cn/download')){
-      imageSrc = imgSrc;
-    }else{
-      imageSrc = await getImageSrc(session, param.slice(1));
-    }
-  }else if(param.length === String(Number(param)).length){
-    imageSrc = `https://q1.qlogo.cn/g?b=qq&nk=${param}&s=640`;
-  }else{
-    console.log('other')
-    const index = param.indexOf('<');
-    const newParam = param.slice(index);
-    if (!newParam?.length) {
-      return '';
-    } else if (param.length === newParam.length) {
-      return getImageSrc(session, newParam.slice(1));
-    }else{
-      return getImageSrc(session, newParam);
+
+  //判断参数是不是纯数字或者没有参数
+  if(param?.length && param?.length === String(Number(param))?.length){
+    return `https://q1.qlogo.cn/g?b=qq&nk=${param}&s=640`;
+  }else if(!param?.length){
+    return `https://q1.qlogo.cn/g?b=qq&nk=${session.userId}&s=640`;
+  }
+  //发送的消息中选择
+  const elementArray = session.elements;
+  for (const element of elementArray) {
+    if (element?.type === 'img'){
+      return element?.attrs?.src;
+    }else if(element?.type === 'at' && element?.attrs?.id && element.attrs.id !== session.selfId){
+      return `https://q1.qlogo.cn/g?b=qq&nk=${element.attrs.id}&s=640`;
     }
   }
-  return imageSrc;
+  //引用的消息中选择
+  const quoteElementArray = session?.quote?.elements;
+  if(quoteElementArray?.length){
+    for (const element of quoteElementArray) {
+      if (element?.type === 'img') {
+        return element?.attrs?.src;
+      } else if (element?.type === 'at' && element?.attrs?.id && element.attrs.id !== session.selfId) {
+        return `https://q1.qlogo.cn/g?b=qq&nk=${element?.attrs?.id}&s=640`;
+      }
+    }
+  }
+  //没有那么返回空值
+  return '';
+
 }
 function initAssets(fileName: string){
   if (fs.existsSync(`${__dirname}/../assets/${fileName}`)) {
@@ -95,7 +97,7 @@ export async function drawLock(baseImage: string) {
     image = await Jimp.read(baseImage);
   }catch(err){
     console.error(err);
-    return '请发送0-1个参数，可以是图片、 @ 某个人或发送 QQ 号'
+    return '发生错误。';
   }
   const size1 = image.width > image.height ? image.height : image.width;
   image.cover({w:size1,h:size1})
@@ -119,7 +121,7 @@ export async function drawSold(baseImage: string) {
     image = await Jimp.read(baseImage);
   }catch(err){
     console.error(err);
-    return '请发送0-1个参数，可以是图片、 @ 某个人或发送 QQ 号'
+    return '发生错误。'
   }
   const size1 = Math.min(image.width,image.height);
   image.cover({w:size1,h:size1})
