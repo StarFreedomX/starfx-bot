fe56e07384cdc149508bdf44477e29a365576232
StarFreedomX <starfreedomx@outlook.com>
2025-04-17 22:52:42 +0800

    加入新功能：我才不是机器人、我喜欢你

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,21 @@ import * as fs from 'fs'
 export const name = 'starfx-bot'
 export let baseDir: string;
 export let assetsDir: string;
+
 export interface Config {
   openLock: boolean,
   openSold: boolean,
   atNotSay: boolean,
+  replyBot: boolean,
+  iLoveYou: boolean,
 }
 
 export const Config: Schema<Config> = Schema.object({
   openLock: Schema.boolean().default(true).description('开启明日方舟封印功能'),
   openSold: Schema.boolean().default(true).description('开启闲鱼"卖掉了"功能'),
   atNotSay: Schema.boolean().default(true).description('开启‘艾特我又不说话’功能'),
+  replyBot: Schema.boolean().default(true).description('开启回复‘机器人’功能'),
+  iLoveYou: Schema.boolean().default(true).description('开启‘我喜欢你’功能')
 })
 
 export const usage = 'StarFreedomX的自用插件 放了一些小功能'
@@ -36,49 +41,79 @@ export function apply(ctx: Context, cfg: Config) {
   }
   if (cfg.openSold) {
     ctx.command('卖掉了 [param]')
-    .action(async ({session}, param) => {
-      console.log('elements');
-      console.log(session.elements);
-      console.log('quote');
-      console.log(session.quote);
-      console.log('param');
-      console.log(param);
-      //console.log(session.selfId);
-      return await drawSold(await getImageSrc(session, param));
-    })
+      .action(async ({session}, param) => {
+        console.log('elements');
+        console.log(session.elements);
+        console.log('quote');
+        console.log(session.quote);
+        console.log('param');
+        console.log(param);
+        //console.log(session.selfId);
+        return await drawSold(await getImageSrc(session, param));
+      })
   }
 
-  if (cfg.atNotSay) {
-    ctx.middleware(async (session, next) => {
-      const elements = session.elements;
-      if (elements.length === 1 && elements[0].type === 'at' && elements[0].attrs.id === session.selfId) {
-        await session.send('艾特我又不说话');
+  ctx.middleware(async (session, next) => {
+    const elements = session.elements;
+    console.log(elements);
+    if (cfg.atNotSay) {
+      if (elements.length === 1 && elements[0].type === 'at') {
+        if (elements[0].attrs.id === session.selfId) {
+          await session.send('艾特我又不说话');
+        }else{
+          await session.send('艾特他又不说话');
+        }
       }
-      return next();
-    })
-  }
+    }
+    if (cfg.replyBot) {
+      const bots = ['bot', '机器人', 'Bot', 'BOT', '机器人！', '机器人!'];
+      const texts = elements?.filter(e => e.type === 'text').map(e => e?.attrs?.content?.trim());
+      const ats = elements?.filter(e => e.type === 'at').map(e => e?.attrs?.id);
+
+      const mentionedBot = texts?.some(t => bots.includes(t));
+      const atMe = ats?.includes(session.selfId);
+      if (
+        (elements?.length === 1 && mentionedBot) ||
+        (elements?.length === 2 && mentionedBot && atMe)
+      ) {
+        await session.send('我才不是机器人！');
+      }
+
+    }
+    if (cfg.iLoveYou) {
+      if (
+        elements?.length === 2 &&
+        elements.some(e => e.type === 'at' && e?.attrs?.id === session.selfId) &&
+        elements.some(e => e.type === 'text' && e?.attrs?.content?.trim() === '我喜欢你')
+      ){
+        await session.send(`${h.quote(session.messageId)}${h.at(session.userId)} 我也喜欢你`)
+      }
+    }
+
+    return next();
+  })
 }
 
-export async function getImageSrc(session: Session, param: string){
+export async function getImageSrc(session: Session, param: string) {
 
   //判断参数是不是纯数字或者没有参数
-  if(param?.length && param?.length === String(Number(param))?.length){
+  if (param?.length && param?.length === String(Number(param))?.length) {
     return `https://q1.qlogo.cn/g?b=qq&nk=${param}&s=640`;
-  }else if(!param?.length){
+  } else if (!param?.length) {
     return `https://q1.qlogo.cn/g?b=qq&nk=${session.userId}&s=640`;
   }
   //发送的消息中选择
   const elementArray = session.elements;
   for (const element of elementArray) {
-    if (element?.type === 'img'){
+    if (element?.type === 'img') {
       return element?.attrs?.src;
-    }else if(element?.type === 'at' && element?.attrs?.id && element.attrs.id !== session.selfId){
+    } else if (element?.type === 'at' && element?.attrs?.id && element.attrs.id !== session.selfId) {
       return `https://q1.qlogo.cn/g?b=qq&nk=${element.attrs.id}&s=640`;
     }
   }
   //引用的消息中选择
   const quoteElementArray = session?.quote?.elements;
-  if(quoteElementArray?.length){
+  if (quoteElementArray?.length) {
     for (const element of quoteElementArray) {
       if (element?.type === 'img') {
         return element?.attrs?.src;
@@ -91,9 +126,10 @@ export async function getImageSrc(session: Session, param: string){
   return '';
 
 }
-function initAssets(fileName: string){
+
+function initAssets(fileName: string) {
   if (fs.existsSync(`${__dirname}/../assets/${fileName}`)) {
-    fs.mkdirSync(`${assetsDir}/`,{recursive: true})
+    fs.mkdirSync(`${assetsDir}/`, {recursive: true})
     fs.copyFileSync(`${__dirname}/../assets/${fileName}`, `${assetsDir}/${fileName}`)
     fs.rmSync(`${__dirname}/../assets/${fileName}`)
   }
@@ -107,20 +143,20 @@ export async function drawLock(baseImage: string) {
   let image;
   try {
     image = await Jimp.read(baseImage);
-  }catch(err){
+  } catch (err) {
     console.error(err);
     return '发生错误。';
   }
   const size1 = image.width > image.height ? image.height : image.width;
-  image.cover({w:size1,h:size1})
+  image.cover({w: size1, h: size1})
   //const middle = new Jimp({width: image.width, height: image.height, color: 0xffffffff});
   const overlay = await Jimp.read(lockUrl);
   //image.resize({w:300,h:300});
-  overlay.resize({w:image.width});
+  overlay.resize({w: image.width});
   //middle.opacity(0.2);
   //image.composite(middle);
   image.composite(overlay);
-  return h.image(await image.getBuffer('image/jpeg'),"image/jpeg");
+  return h.image(await image.getBuffer('image/jpeg'), "image/jpeg");
 }
 
 export async function drawSold(baseImage: string) {
@@ -131,17 +167,17 @@ export async function drawSold(baseImage: string) {
   let image;
   try {
     image = await Jimp.read(baseImage);
-  }catch(err){
+  } catch (err) {
     console.error(err);
     return '发生错误。'
   }
-  const size1 = Math.min(image.width,image.height);
-  image.cover({w:size1,h:size1})
+  const size1 = Math.min(image.width, image.height);
+  image.cover({w: size1, h: size1})
   const middle = new Jimp({width: image.width, height: image.height, color: 0xffffffff});
   const overlay = await Jimp.read(soldUrl);
-  overlay.resize({w:image.width * 182 / 240});
+  overlay.resize({w: image.width * 182 / 240});
   middle.opacity(0.4);
   image.composite(middle);
   image.composite(overlay, image.width * 29 / 240, image.width * 29 / 240);
-  return h.image(await image.getBuffer('image/jpeg'),"image/jpeg");
+  return h.image(await image.getBuffer('image/jpeg'), "image/jpeg");
 }
